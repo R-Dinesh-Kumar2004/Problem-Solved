81bbfbe9f9b09c51142ce1f07fb228e06711822a
Dinesh Kumar R <dineshkumarraja2004@gmail.com>
2024-12-23 07:34:43 +0530

    Time: 38 ms (91.78%), Space: 59.6 MB (93.84%) - LeetHub

---
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    class Pair{
+        int val;
+        int idx;
+
+        Pair(int val,int idx){
+            this.val=val;
+            this.idx=idx;
+        }
+    }
+    public int minimumOperations(TreeNode root) {
+
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        int count=0;
+        
+        while(!q.isEmpty()){
+            int n=q.size();
+            Pair arr[]=new Pair[n];
+            for(int i=0;i<n;i++){
+                TreeNode t=q.poll();
+                if(t.left!=null)q.offer(t.left);
+                if(t.right!=null)q.offer(t.right);
+                arr[i]=new Pair(t.val,i);
+            }
+            Arrays.sort(arr,(a,b)->a.val-b.val);
+            for(int i=0;i<n;i++){
+                if(arr[i].idx!=i){
+                    count++;
+                    swap(arr,i,arr[i].idx);
+                    i--;
+                }
+            }
+        }
+        return count;
+    }
+    public void swap(Pair arr[],int i,int j){
+        Pair t=arr[i];
+        arr[i]=arr[j];
+        arr[j]=t;
+    }
+}
