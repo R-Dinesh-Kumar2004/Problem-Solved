b00088a6f825d29210e21486a4916b80fd612018
Dinesh Kumar R <dineshkumarraja2004@gmail.com>
2025-01-02 14:01:41 +0530

    Time: 32 ms (5.47%), Space: 85.8 MB (48.29%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -1,28 +1,46 @@
 class Solution {
+    private static  int st[];
     public int[] vowelStrings(String[] words, int[][] queries) {
-        int n=words.length;
-        int pre[] = new int[n+1];
-        for(int i=0;i<n;i++){
-            int cnt=countVowels(words[i]);
-            pre[i+1]=pre[i]+cnt;
-            // System.out.print(pre[i+1]+" ");
-        }
-        int idx=0;
+        int n = words.length;
+        st = new int[4*n];
+        buildSeg(0,0,n-1,words);
+
         int ans[] = new int[queries.length];
-        for(int i[]:queries){
-            int tot=pre[i[1]+1]-pre[i[0]];
-            ans[idx++]=tot;
+        int i=0;
+        for(int q[]:queries){
+            ans[i++]=query(0,0,n-1,q[0],q[1]);
         }
         return ans;
     }
+    public static void buildSeg(int i,int low,int high,String words[]){
+        if(low==high){
+            st[i]=isValid(words[low]);
+            return;
+        }
+        int mid=(low+high)/2;
+        buildSeg(2*i+1,low,mid,words);
+        buildSeg(2*i+2,mid+1,high,words);
 
-    public static int countVowels(String s){
+        st[i]=st[2*i+1]+st[2*i+2];
+    }
+
+    public static int isValid(String s){
         int n=s.length();
         if(isVowel(s.charAt(0)) && isVowel(s.charAt(n-1))) return 1;
         return 0;
-    }
+    } 
 
     public static boolean isVowel(char c){
         return c=='a'||c=='e'||c=='i'||c=='o'||c=='u';
+    }
+
+    public static int query(int i,int low,int high,int l,int r){
+        if(low>=l && r>=high) return st[i];
+        if(low>r || high<l) return 0;
+        int mid=(low+high)/2;
+        int left=query(2*i+1,low,mid,l,r);
+        int right=query(2*i+2,mid+1,high,l,r);
+
+        return left+right;
     }
 }
