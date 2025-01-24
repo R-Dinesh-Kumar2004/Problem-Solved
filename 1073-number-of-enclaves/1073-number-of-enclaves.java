fbd9a8c1eb758d85d0ea0a011b82824aa9570d9f
Dinesh Kumar R <dineshkumarraja2004@gmail.com>
2025-01-24 14:08:30 +0530

    Time: 9 ms (59.32%), Space: 55.1 MB (70.07%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.java
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -4,20 +4,14 @@ class Solution {
         int n = grid[0].length;
         boolean vis[][] = new boolean[m][n];
         Queue<int[]> q = new LinkedList<>();
-        for(int i=0;i<n;i++){
-            if(grid[0][i]==1){
-                q.add(new int[]{0,i}); vis[0][i] = true;
-            }
-            if(grid[m-1][i]==1){
-                q.add(new int[]{m-1,i}); vis[m-1][i] = true;
-            }
-        }
-        for(int i=1;i<m-1;i++){
-            if(grid[i][0]==1){
-                q.add(new int[]{i,0}); vis[i][0] = true;
-            }
-            if(grid[i][n-1]==1){
-                q.add(new int[]{i,n-1}); vis[i][n-1] = true;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0 || i==m-1 || j==n-1){
+                    if(grid[i][j]==1){
+                        q.add(new int[]{i,j});
+                        vis[i][j] = true;
+                    }
+                }
             }
         }
         int dx[] = {-1,1,0,0};
