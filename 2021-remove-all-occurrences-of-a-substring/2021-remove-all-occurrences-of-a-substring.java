0eca78a12e2c9e15321ef8bdf1ad458951d09994
Dinesh Kumar R <dineshkumarraja2004@gmail.com>
2025-02-11 07:31:47 +0530

    Time: 4 ms (49.85%), Space: 41.9 MB (88.37%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -4,19 +4,16 @@ class Solution {
         int n = part.length();
         int top = -1;
         for(char c : s.toCharArray()){
-            int flag = 0;
+            st[++top] = c;
             if(c == part.charAt(n-1)){
-                int t = top, i = n-2;
-                flag = 1;
+                int t = top, i = n-1;
                 while(top>=0 && i>=0 && st[top] == part.charAt(i)){
                     i--;top--;
                 }
                 if(i >= 0){
                    top = t;
-                   flag = 0;
                 } 
             }
-            if(flag == 0) st[++top] = c;
         }
         // System.out.print(top+" ");
         StringBuffer sb = new StringBuffer();
