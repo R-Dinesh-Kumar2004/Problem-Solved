e101e7e21cc138d9f27f08f03962495ba0413c06
Dinesh Kumar R <dineshkumarraja2004@gmail.com>
2025-03-14 07:44:48 +0530

    Time: 26 ms (39.27%), Space: 108.6 MB (80.51%) - LeetHub

---
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minZeroArray(int[] nums, int[][] queries) {
+        int ans = -1;
+        int high = queries.length-1;
+        int low = 0;
+
+        if(Arrays.equals(nums,new int[nums.length])) return 0;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(isValid(queries,nums,mid)){
+                ans = mid+1;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return ans;
+    }
+    public boolean isValid(int queries[][],int nums[],int sz){
+        int n = nums.length;
+        int temp[] = new int[n];
+
+        for(int i=0;i<=sz;i++){
+            int s = queries[i][0], e = queries[i][1], val = queries[i][2];
+            temp[s] += val;
+            if(e+1<n) temp[e+1] -= val;
+        }
+        if(nums[0]>temp[0]) return false;
+        int sum = temp[0];
+        for(int i=1;i<n;i++){
+            temp[i] += temp[i-1];
+            if(temp[i]<nums[i]) return false;
+            // System.out.print(sum+" ");
+        }
+        // System.out.println();
+        return true;
+    }
+}
