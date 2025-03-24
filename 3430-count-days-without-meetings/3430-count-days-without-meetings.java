40bc15db178a431e99a61a1ba82a07aeaff4225a
Dinesh Kumar R <dineshkumarraja2004@gmail.com>
2025-03-24 06:57:37 +0530

    Time: 35 ms (95.96%), Space: 100.6 MB (48.82%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.java
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.java
@@ -1,15 +1,16 @@
 class Solution {
     public int countDays(int days, int[][] meetings) {
+        int count = 0, last = 1;
         Arrays.sort(meetings,(a,b)->a[0]-b[0]);
-        int tot=0;
-        int prev=1;
-        for(int i[]:meetings){
-            if(i[0]>prev){
-                tot+=i[0]-prev;
-            }
-            prev=Math.max(prev,i[1]+1);
+
+        for(int meet[] : meetings){
+           if(last<meet[0]){
+            count += meet[0] - last;
+           }
+           last = Math.max(last,meet[1]+1);
         }
-        if(days>=prev)tot+=days-prev+1;
-        return tot;
+        if(days>=last) count += days-last+1;
+
+        return count;
     }
 }
