ef89de6f8b6af4d97d82609dcd8f495907156b36
Dinesh Kumar R <dineshkumarraja2004@gmail.com>
2025-04-18 13:38:39 +0530

    Time: 6 ms (49.76%), Space: 41.5 MB (60.37%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,25 +1,21 @@
 class Solution {
     public String countAndSay(int n) {
-        return rec("1",n-1);
-    }
-    public static String rec(String t,int n){
-        if(n==0) return t;
-        t=com(t);
-        return rec(t,n-1);
-    }
-
-    public static String com(String s){
-        int count=1;
-        StringBuffer sb = new StringBuffer("");
-        int n=s.length();
+        String s = "1";
         for(int i=1;i<n;i++){
-            if(s.charAt(i)==s.charAt(i-1)) count++;
-            else{
-                sb.append(count).append(s.charAt(i-1));
-                count=1;
+            StringBuffer sb = new StringBuffer();
+            int len = s.length(), count = 1;
+            char prev = s.charAt(0);
+            for(int j=1;j<len;j++){
+                if(s.charAt(j) == prev) count++;
+                else{
+                    sb.append(count).append(prev);
+                    count = 1;
+                }
+                prev = s.charAt(j);
             }
+            sb.append(count).append(prev);
+            s = sb.toString();
         }
-        sb.append(count).append(s.charAt(n-1));
-        return sb.toString();
+        return s;
     }
 }
