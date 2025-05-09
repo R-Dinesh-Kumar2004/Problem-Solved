bc6116cece3388cfdcb4574afdf73e8ed1574afb
Dinesh Kumar R <dineshkumarraja2004@gmail.com>
2025-05-09 14:29:01 +0530

    Time: 184 ms (88.1%), Space: 111.8 MB (37.7%) - LeetHub

--- 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.java
+++ 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.java
@@ -1,55 +1,33 @@
 class Solution {
-
-    private static final int INF = 0x3f3f3f3f;
-
-    class State implements Comparable<State> {
-
-        int x, y, dis;
-
-        State(int x, int y, int dis) {
-            this.x = x;
-            this.y = y;
-            this.dis = dis;
-        }
-
-        @Override
-        public int compareTo(State other) {
-            return Integer.compare(this.dis, other.dis);
-        }
-    }
-
     public int minTimeToReach(int[][] moveTime) {
-        int n = moveTime.length, m = moveTime[0].length;
-        int[][] d = new int[n][m];
-        boolean[][] v = new boolean[n][m];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(d[i], INF);
-        }
+        int m = moveTime.length;
+        int n = moveTime[0].length;
+        int dx[] = {1,-1,0,0};
+        int dy[] = {0,0,-1,1};
+        boolean vis[][] = new boolean[m][n];
 
-        int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
-        d[0][0] = 0;
-        PriorityQueue<State> q = new PriorityQueue<>();
-        q.offer(new State(0, 0, 0));
-        while (!q.isEmpty()) {
-            State s = q.poll();
-            if (v[s.x][s.y]) continue;
-            if (s.x == n - 1 && s.y == m - 1) break;
-            v[s.x][s.y] = true;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[2]-b[2]);
+        pq.add(new int[]{0,0,0,0});
+        vis[0][0] = true;
 
-            for (int[] dir : dirs) {
-                int nx = s.x + dir[0];
-                int ny = s.y + dir[1];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                int dist =
-                    Math.max(d[s.x][s.y], moveTime[nx][ny]) +
-                    ((s.x + s.y) % 2) +
-                    1;
-                if (d[nx][ny] > dist) {
-                    d[nx][ny] = dist;
-                    q.offer(new State(nx, ny, dist));
+        while(!pq.isEmpty()){
+            int top[] = pq.poll();
+            int x = top[0],y = top[1],wt = top[2];
+            
+            if(x == m-1 && y == n-1) return wt;
+
+            for(int k=0;k<4;k++){
+                int i = dx[k]+x;
+                int j = dy[k]+y;
+
+                if(i>=0 && j>=0 && i<m && j<n && !vis[i][j]){
+                    vis[i][j] = true;
+                    int t = (wt<moveTime[i][j]) ? moveTime[i][j] : wt;
+                    if(top[3] == 1) t += 1;
+                    pq.add(new int[]{i,j,t+1,1-top[3]});
                 }
             }
         }
-        return d[n - 1][m - 1];
+        return -1;
     }
 }
