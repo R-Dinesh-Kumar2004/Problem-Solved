d41ed77bc0ddce24be982de549142606510dbbb2
Dinesh Kumar R <dineshkumarraja2004@gmail.com>
2025-07-22 23:30:44 +0530

    Time: 117 ms (7.02%), Space: 57 MB (43.63%) - LeetHub

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minimizedMaximum(int n, int[] quantities) {
+        int low = 1;
+        int high = Arrays.stream(quantities).max().getAsInt();
+
+        int ans = 0;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(isPossible(n,mid,quantities)){
+                ans = mid;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+
+        return ans;
+    }
+    public boolean isPossible(int n,int distribute,int quantities[]){
+        int count = 0;
+
+        for(int val : quantities){
+            count += Math.ceil((double)val/distribute);
+        }
+
+        return count <= n;
+    }
+}
