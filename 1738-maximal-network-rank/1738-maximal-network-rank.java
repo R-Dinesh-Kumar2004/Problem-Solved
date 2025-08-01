e7183d12397c6629603b3ebb64d04cb011365327
Dinesh Kumar R <dineshkumarraja2004@gmail.com>
2025-08-01 14:44:43 +0530

    Time: 4 ms (97.68%), Space: 46.2 MB (19.62%) - LeetHub

--- 1738-maximal-network-rank/1738-maximal-network-rank.java
+++ 1738-maximal-network-rank/1738-maximal-network-rank.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
-        int adjMat[][] = new int[n+1][n+1];
+        boolean adjMat[][] = new boolean[n][n];
         int indeg[] = new int[n];
 
         for(int road[] : roads){
@@ -8,16 +8,15 @@ class Solution {
             indeg[u]++; 
             indeg[v]++;
 
-            adjMat[u][v] = 1;
-            adjMat[v][u] = 1;
+            adjMat[u][v] = true;
+            adjMat[v][u] = true;
         } 
 
         int ans = 0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i == j) continue;
+            for(int j=i+1;j<n;j++){
                 int cur = indeg[i] + indeg[j];
-                if(adjMat[i][j] == 1) cur -= 1;
+                if(adjMat[i][j]) cur -= 1;
                 ans = Math.max(ans,cur);
             }
         }
