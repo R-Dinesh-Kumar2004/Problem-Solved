c8c666c3c5181a6a7ed10c6d28a3014866ab26af
Dinesh Kumar R <dineshkumarraja2004@gmail.com>
2025-09-25 20:05:58 +0530

    Time: 3 ms (79.02%), Space: 45.3 MB (69.84%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -2,9 +2,19 @@ class Solution {
     int sum = 0;
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int dp[][] = new int[n][n];
-        for(int it[]:dp) Arrays.fill(it,-1);
-        return rec(0,0,n,triangle,dp);
+        int[][] dp = new int[n][n];
+        
+        for (int j = 0; j < n; j++) {
+            dp[n-1][j] = triangle.get(n-1).get(j);
+        }
+        
+        for (int i = n-2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+        
+        return dp[0][0];
     }
     public int rec(int i,int j,int n,List<List<Integer>> tri,int dp[][]){
         if(i==n-1){
