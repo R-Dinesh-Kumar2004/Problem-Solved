b865d9343b7f991911ad9521dd15b8d4fa4b89a2
Dinesh Kumar R <dineshkumarraja2004@gmail.com>
2025-10-24 17:48:55 +0530

    Time: 89 ms (67.69%), Space: 44.4 MB (69.23%) - LeetHub

--- 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.java
+++ 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.java
@@ -1,23 +1,21 @@
 class Solution {
     public int nextBeautifulNumber(int n) {
         for(int i=n+1;i<10000000;i++){
-            if(isBalanced(i)) return i;
+            if(isValid(i)) return i;
         }
         return -1;
     }
-    public boolean isBalanced(int n){
+    public boolean isValid(int num){
         int freq[] = new int[10];
-
-        while(n > 0){
-            int rem = n%10;
-            freq[rem]++;
-            n /= 10;
+        while(num > 0){
+            freq[num%10]++;
+            num /= 10;
         }
 
         for(int i=0;i<10;i++){
-            if(freq[i]>0 && i != freq[i]) return false;
+            if(freq[i] == 0) continue;
+            if(freq[i] != i) return false;
         }
-
         return true;
     }
 }
