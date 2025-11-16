923cf1912ba72426d7188948259c287695636c25
Dinesh Kumar R <dineshkumarraja2004@gmail.com>
2025-11-16 11:20:43 +0530

    Time: 3 ms (100%), Space: 47 MB (0%) - LeetHub

---
+++ 4090-minimum-string-length-after-balanced-removals/4090-minimum-string-length-after-balanced-removals.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minLengthAfterRemovals(String s) {
+        int aCount = 0, bCount = 0;
+
+        for(char c : s.toCharArray()){
+            if(c == 'a') aCount++;
+            else bCount++;
+        }
+        return Math.abs(aCount-bCount);
+    }
+}
